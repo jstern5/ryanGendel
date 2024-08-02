d7885a2c09eab81f6c0eb562044fea63507a2209
jstern5 <45157848+jstern5@users.noreply.github.com>
2024-08-01 21:25:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+console.log('You have connected...')
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+    let generateBtn = document.querySelector('#generate-pokemon');
+    generateBtn.addEventListener('click', renderEverything)
+
+    getDeleteBtn().addEventListener('click', deleteEverything);
+})
+
+function renderEverything(){
+    let allPokemonContainer = document.querySelector('#poke-container')
+    allPokemonContainer.innerText = "";
+    fetchKantoPokemon();
+
+    getDeleteBtn().style.display = 'block'
+}
+
+function getDeleteBtn(){
+    return document.querySelector('#delete-btn')
+}
+
+
+function fetchKantoPokemon(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(response => response.json())
+    .then(function(allpokemon){
+        allpokemon.results.forEach(function(pokemon){
+            fetchPokemonData(pokemon);
+        })
+    })
+}
+
+// Saves the Pokemon URL to use for FETCH.
+
+function fetchPokemonData(pokemon){
+    let url = pokemon.url  
+                                
+    fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData){
+        renderPokemon(pokeData)
+    })
+}
+
+
+function renderPokemon(pokeData){
+    let allPokemonContainer = document.getElementById('poke-container');
+	//Holds the data/details for indiviual pokemon.{}
+    let pokeContainer = document.createElement("div") 
+    pokeContainer.classList.add('ui', 'card');
+
+    createPokeImage(pokeData.id, pokeContainer);
+
+    let pokeName = document.createElement('h4') 
+    pokeName.innerText = pokeData.name
+
+    let pokeNumber = document.createElement('p')
+    pokeNumber.innerText = `#${pokeData.id}`
+   
+	//Holds the pokemon types
+    let pokeTypes = document.createElement('ul') 
+  
+// Goes through the types arrays and creates li tags for each Pokemon
+    createTypes(pokeData.types, pokeTypes) 
+
+	//Appends the details of the Pokemon to the pokeContainer div
+    pokeContainer.append(pokeName, pokeNumber, pokeTypes);   
+    allPokemonContainer.appendChild(pokeContainer);      
+	
+	//Appends the pokeContainer div to the div that will hold the Pokemon cards
+}
+
+function createTypes(types, ul){
+    types.forEach(function(type){
+        let typeLi = document.createElement('li');
+        typeLi.innerText = type['type']['name'];
+        ul.append(typeLi)
+    })
+}
+
+function createPokeImage(pokeID, containerDiv){
+    let pokeImgContainer = document.createElement('div')
+    pokeImgContainer.classList.add('image')
+
+    let pokeImage = document.createElement('img')
+    pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+
+    pokeImgContainer.append(pokeImage);
+    containerDiv.append(pokeImgContainer);
+}
+
+function deleteEverything(event){
+    event.target.style = 'none';
+    let allPokemonContainer = document.querySelector('#poke-container')
+    allPokemonContainer.innerText = ""
+
+    let generateBtn = document.createElement('button')
+    generateBtn.innerText = "Generate Pokemon"
+    generateBtn.id = 'generate-pokemon'
+    generateBtn.classList.add('ui', 'secondary', 'button')
+    generateBtn.addEventListener('click', renderEverything);
+
+    allPokemonContainer.append(generateBtn)
+}
---
+++ pokemon.js
@@ -0,0 +1,90 @@
+// Define the API URL
+const apiUrl = 'https://pokeapi.co/';
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+const apiUrl = 'https://pokeapi.co/';
+const outputElement = document.getElementById('output');
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Display data in an HTML element
+    outputElement.textContent = JSON.stringify(data, null, 2);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+const apiUrl = 'https://pokeapi.co/';
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('Data not found');
+      } else if (response.status === 500) {
+        throw new Error('Server error');
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    }
+    return response.json();
+  })
+  .then(data => {
+    outputElement.textContent = JSON.stringify(data, null, 2);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+const apiUrl = 'https://pokeapi.co/';
+const data = {
+  name: 'John Doe',
+  email: 'johndoe@example.com',
+};
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+};
+
+fetch(apiUrl, requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    outputElement.textContent = JSON.stringify(data, null, 2);
+  })
+  .catch(error => {
+    console.error
+
+('Error:', error);
+  });
